14d6fd21b6c8784864c1f82fc75413b8bf9eb731
bjoekris <bbkspil@gmail.com>
2024-11-22 11:44:22 +0100

    Adding basic imports and run code for FastAPI

---
+++ Api.py
@@ -0,0 +1,17 @@
+#pip install "fastapi[standard]"
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+
+"""
+@app.post("/api")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+"""
