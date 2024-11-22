5cba4ecd8eca168add1b0a909b7b304f53bee6ec
Magnus N <magnusnaehr@live.dk>
2024-11-22 15:41:18 +0100

    Update to template builder

--- TemplateMakerAPI.py
+++ TemplateMakerAPI.py
@@ -1,13 +1,16 @@
 # pip install "fastapi[standard]"
 
+from SaveToPDF import DocxToPDF
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import docx
 from docx import Document
 from docx.shared import Inches
-from docx import Document
 from docx.shared import Pt, RGBColor
+
+import winreg
 
 
 
@@ -21,10 +24,10 @@ if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
 """
 
-def MakeTemplate(name, email, product, unit, price):
+def MakeTemplate(name, product, unit, price):
     doc = Document()
-    h1 = doc.add_heading('Invoice', 0)
-    p1 = doc.add_paragraph(f'Dear {name} ,')
+    doc.add_heading('Invoice', 0)
+    doc.add_paragraph(f'Dear {name} ,')
     p2 = doc.add_paragraph('Please find attached invoice for your recent purchase of ')
     p2.add_run(str(unit)).bold = True
     p2.add_run(' units of ')
@@ -48,12 +51,21 @@ def MakeTemplate(name, email, product, unit, price):
     rowCells[2].text = f'{price:,.2f}'
     rowCells[3].text = f'{unit * price:,.2f}'
 
-    doc.add_page_break
+    [doc.add_paragraph('') for _ in range(20)]
 
     doc.add_paragraph('We apprecieate your business and please come again!')
     doc.add_paragraph('Sincerely')
     doc.add_paragraph('InvoEZ')
 
-    doc.save(f'{name}.docx')
+    reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
 
-MakeTemplate('Cadana', 'cadana.mail.com', 'some product', 100, 1000)
+    folderPath = winreg.QueryValueEx(reg_key, "{374DE290-123F-4565-9164-39C4925E467B}")[0]
+
+    winreg.CloseKey(reg_key)
+
+    path = f'{folderPath}/{name}'
+
+    doc.save(f'{path}.docx')
+    DocxToPDF(path)
+
+MakeTemplate('Cadana', 'some product', 100, 1000)
