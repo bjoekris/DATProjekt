8d977b0f669bd9546b1ecaae5b86ecafba8ffb9b
Magnus N <magnusnaehr@live.dk>
2024-12-03 14:47:08 +0100

    DocxTPL updated, still not functional

--- docxtpl/docxTemplateTest.py
+++ docxtpl/docxTemplateTest.py
@@ -1,5 +1,6 @@
 # pip install 'fastapi[standard]'
 # pip install docxtpl
+# pip install docx2pdf
 
 # Used for API calls
 from fastapi import FastAPI, Form, File
@@ -9,6 +10,10 @@ from fastapi.responses import FileResponse
 # Used for inserting data in template
 from docxtpl import DocxTemplate
 from datetime import datetime
+
+# Used in ConvertDocxToPDF()
+import os
+from docx2pdf import convert
 
 app = FastAPI()
 
@@ -42,7 +47,18 @@ async def inser_dynamic_data(
     tpl.render({'{{Current Date}}' : datetime.today.strftime("%d-%m-%Y")})
     for table in tables: tpl.render(tables[table])
 
-    tpl.save(f'{outputPath}.pdf')
+    tpl.save(f'{outputPath}.docx')
+    ConvertDocxToPDF(outputPath)
+
+def ConvertDocxToPDF(path):
+    convert(f'{path}.docx', f'{path}.pdf')
+    os.remove(f'{path}.docx')
+
+    return FileResponse(f'{path}.pdf', media_type = 'application/pdf', filename = path)
+
+
+# ------------------------------------------------------------------------------------------- #
+
 
 def InserDynamicData(
         inputPath: str,
@@ -51,14 +67,39 @@ def InserDynamicData(
         context: dict[str, str],
         tables: dict[dict[str, list[int, float]]]
     ):
-
-    tpl = DocxTemplate(f'{inputPath}.docx')
+    tpl = DocxTemplate(f'docxtpl/{inputPath}.docx')
 
     tpl.render(context)
-    tpl.render({'{{Current Date}}' : datetime.today.strftime("%d-%m-%Y")})
+    tpl.render({'{{Current Date}}' : datetime.today().strftime("%d-%m-%Y")})
     for table in tables: tpl.render(tables[table])
 
     tpl.save(f'{outputPath}.pdf')
+
+
+# ------------------------------------------------------------------------------------------- #
+
+
+# Example of how the context dictionary could look like:
+context = {
+    '{{Name}}' : 'Magnus Næhr',
+    '{{Adress}}' : 'Mølletoften 1',
+    '{{City_postcode}}' : 'Lyngby, 2800',
+}
+
+# Example for how a table could look like in the table dictionary:
+priceTabel = {
+    'listEntries' : [
+        { 'Fast pris for flytning' : [1, 9999.95] },
+        { 'Fast pris for nedpakning' : [1, 249.95] },
+        { 'Fast pris for opbevaring' : [1, 349.95] },
+        { 'Pris for udlejning af udstyr' : [8, 199.95] },
+        { 'Tungløft' : [7, 99.95] }
+    ]
+}
+
+
+# ------------------------------------------------------------------------------------------- #
+
 
 if __name__ == '__main__':
     usingPostman = 0
@@ -67,20 +108,4 @@ if __name__ == '__main__':
         import uvicorn
         uvicorn.run(app, host = '127.0.0.1', port = 8000)
     else:
-        InserDynamicData('HC Andersen Flyttefirma Template', 'HC Andersen Flyttefirma Invoice',
-                         {'{{Name}}' : 'Magnus Næhr', '{{Adress}}' : 'Mølletoften 1', '{{City, postcode}}' : 'Lyngby, 2800'},
-                         {'items' : {'Fast pris for flytning' : [1, 9999.95], 'Fast pris for nedpakning' : [1, 249.95], 'Fast pris for udpakning' : [1, 299.95],
-                                     'Fast pris for opbevaring' : [1, 349.95], 'Pris for udlejning af udstyr' : [8, 199.95], 'Pris for tungløft' : [7, 99.95]}})
-
-# Example for how a table could look like:
-tables = [
-    {
-#       String, indicating name of cell row.
-#       List should be understood as this: [units, price per unit].
-        'Fast pris for flytning' : [1, 9999.95],
-        'Fast pris for nedpakning' : [1, 249.95],
-        'Fast pris for opbevaring' : [1, 349.95],
-        'Pris for udlejning af udstyr' : [8, 199.95],
-        'Tungløft' : [7, 99.95]
-    }
-]
+        InserDynamicData('HC Andersen Flyttefirma Template', 'HC Andersen Flyttefirma Invoice', context, priceTabel)
