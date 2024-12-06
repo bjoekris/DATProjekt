0e326cc71a85835846b9346a2f4214b379a6c308
Magnus N <magnusnaehr@live.dk>
2024-12-06 13:48:37 +0100

    Unit tests for docxtpl test

--- docxtpl/docxTemplateTest.py
+++ docxtpl/docxTemplateTest.py
@@ -15,10 +15,15 @@ import shutil
 # Used for inserting data in template
 from docxtpl import DocxTemplate
 from docx import Document
+from http import HTTPStatus
 
 # Used for converting docx-format to pdf-format
 import os
 from docx2pdf import convert
+
+# Used for unit testing
+import unittest
+from copy import deepcopy
 
 app = FastAPI()
 
@@ -31,25 +36,27 @@ app.add_middleware(
 )
 
 @app.post('/insert-dynamic-data/')
-async def inser_dynamic_data(
+async def insert_dynamic_data(
         templateFile: UploadFile = File(...),
         pdfName: str = Form(...),
         contextFile: UploadFile = File(...),
+        images: list[UploadFile] = File(None),
     ):
     # Extracts the context-dictionary from .json file
     context = json.loads(contextFile.file.read())
-
+    
     # Save a copy pf the uploaded template file
     templatePath = 'uploadedTemplate.docx'
     with open(templatePath, "wb") as buffer:
         shutil.copyfileobj(templateFile.file, buffer)
-
+    
     tpl = DocxTemplate(templatePath)
+
     context = InitializeContext(context)
     
-    errMsg, variablesValid = ValidateVariables(templatePath, context)
-    if variablesValid == False:
-        return errMsg
+    errMsg, valid = ValidateVariables(templatePath, context)
+    if valid == False:
+        return ValueError, errMsg
 
     # Inserts data from the context-dictionary to the template
     tpl.render(context)
@@ -62,7 +69,7 @@ def ConvertDocxToPDF(path: str):
     convert(f'{path}.docx', f'{path}.pdf')
     os.remove(f'{path}.docx')
 
-    return FileResponse(f'{path}.pdf', media_type = 'application/pdf', filename = path)
+    return True, FileResponse(f'{path}.pdf', media_type = 'application/pdf', filename = path)
 
 def ValidateVariables(path: str, context: dict):
     doc = Document(path)
@@ -112,18 +119,18 @@ def ValidateVariables(path: str, context: dict):
                 valuesNotInputted.remove(value)
     
     # Builds the exception string, so it can be returned
-    exception = 'ContextVariablesError: '
+    errorMsg = ''
     if not len(keysNotContained) == 0:
         for key in keysNotContained:
-            exception += f'{key} was not found in template. '
+            errorMsg += f'{key} was not found in template. '
         valid = False
     
     if not len(valuesNotInputted) == 0:
         for value in valuesNotInputted:
-            exception += f'{value} was not found in context. '
+            errorMsg += f'{value} was not found in context. '
         valid = False
     
-    return exception, valid
+    return errorMsg, valid
 
 # Inserts the total price to the context dictionary, this is done to automate the proccess
 # I would suggest moving this out of the python file and place it in the WebApp
@@ -184,8 +191,132 @@ def InitializeContext(context):
     return context
 
 # ------------------------------------------------------------------------------------------- #
+# ----------------------------------------Unit Tests:---------------------------------------- #
+
+
+contextDict = {
+    "Name" : "Magnus Næhr",
+    "Adress" : "Mølletoften 1",
+    "City_postcode" : "Lyngby, 2800",
+    "Customer_Number" : 123456,
+    "Order_Number" : 123456,
+    "Current_Date" : "05-12-2024",
+    "Invoice_Type" : "Ordrebekræftelse",
+    "Offer_Name" : "Dette er tilbudet",
+    "Date_of_Execution" : "13-12-2024",
+    "Floor_Elevator" : "2. etage, m. elevator.",
+    "Square_Meters" : 28,
+    "Parking" : "Perkeringsplads ude foran bolig.",
+    "Time_Estimation" : "2 timer.",
+    "Task_Description" : "Dette er opgave beskrivelsen.",
+    "Second_Adresses" : [
+        "Dette er en sekundær addresse.",
+        "Dette er også en sekundær adresse."
+    ],
+    "Comments" : [
+        "Dette er en kommentar",
+        "Dette er også en kommentar"
+    ],
+    "Agreed_Date" : "13-12-2024",
+    "Start_Time" : "10:30 - 11:30",
+    "Agreed_Date_Equipment" : "14-12-2024",
+    "Start_Time_Equipment" : "11:00 - 12:00",
+    "Customer_Phone" : "12345678",
+    "Total_Price" : 0,
+    "Reg_Number" : 123456,
+    "Account_Number" : 123456,
+    "Order_Id" : 123456,
+    "Details" : [
+        "Dette er en detalje.",
+        "Dette er også en detalje"
+    ],
+    "itemsTable" : [
+        {
+            "type" : "Fast pris for flytning",
+            "units" : 1,
+            "priceUnit" : 9999.95,
+            "priceTotal" : 0
+        },
+        {
+            "type" : "Fast pris for nedpakning",
+            "units" : 1,
+            "priceUnit" : 249.95,
+            "priceTotal" : 0
+        },
+        {
+            "type" : "Fast pris for opbevaring",
+            "units" : 1,
+            "priceUnit" : 349.95,
+            "priceTotal" : 0
+        },
+        {
+            "type" : "Pris for udlejning af udstyr",
+            "units" : 8,
+            "priceUnit" : 199.95,
+            "priceTotal" : 0
+        },
+        {
+            "type" : "Tungløft",
+            "units" : 7,
+            "priceUnit" : 99.95,
+            "priceTotal" : 0
+        }
+    ]
+}
+
+testTemplatePath = 'docxtpl/HC Andersen Flyttefirma Template.docx'
+testPDFName = 'HC Andersen Flyttefirma Invoice'
+
+def test_InsertDynamicData(
+        templateFile: str,
+        pdfName: str,
+        contextFile: dict,
+    ):
+    context = contextFile
+
+    templatePath = 'uploadedTemplate.docx'
+    tpl = DocxTemplate(templateFile)
+    
+    context = InitializeContext(context)
+    
+    errMsg, valid = ValidateVariables(templatePath, context)
+    if valid == False:
+        return ValueError, errMsg
+
+    # Inserts data from the context-dictionary to the template
+    tpl.render(context)
+    
+    tpl.save(f'{pdfName}.docx')
+    InsertPageNumbers(f'{pdfName}.docx')
+    return ConvertDocxToPDF(pdfName)
+
+class TestInsertDynamicData(unittest.TestCase):
+    def test_contextTooLarge(self):
+        testContext = deepcopy(contextDict)
+        expectedErrMsg = ValueError, 'test1 was not found in template. '
+
+        testContext['test1'] = 1
+        self.assertEqual(test_InsertDynamicData(testTemplatePath, testPDFName, testContext), expectedErrMsg)
+    
+    def test_contextTooSmall(self):
+        testContext = deepcopy(contextDict)
+        expectedErrMsg = ValueError, 'Name was not found in context. '
+
+        testContext.pop('Name')
+        self.assertEqual(test_InsertDynamicData(testTemplatePath, testPDFName, testContext), expectedErrMsg)
+    
+    def test_success(self):
+        self.assertTrue(test_InsertDynamicData(testTemplatePath, testPDFName, contextDict))
+
+
+# ------------------------------------------------------------------------------------------- #
 
 
 if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host = '127.0.0.1', port = 8000)
+    unitTest = 0
+
+    if unitTest == 1:
+        unittest.main()
+    else:
+        import uvicorn
+        uvicorn.run(app, host = '127.0.0.1', port = 8000)
