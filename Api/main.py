87f66d49d6380c59cd0dae0514a08f3b9f72af6b
Bjørn Kristensen <bbkspil@gmail.com>
2024-12-13 13:09:03 +0100

    API-Key acces now works in main.py

--- Api/main.py
+++ Api/main.py
@@ -1,8 +1,10 @@
 # Brugt til API kald
-from fastapi import FastAPI, Form, File, UploadFile
+from fastapi import FastAPI, Form, File,Header,HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import uvicorn
+
+import logging
 
 # Brugt til i konversionen af nødvændig data, ved konvertering fra .json til given datatype
 import json
@@ -19,6 +21,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+#Loggin setup
+logging.basicConfig(level=logging.INFO)
+
+
+# API keys for validation
+API_KEYS = {
+    "user1": "abc123456789",
+    "user2": "def987654321",
+}
+
+# Function to validate API key
+def validate_api_key(api_key: str) -> bool:
+    return api_key in API_KEYS.values()
 ## --------------------------------------------------------------------------------------------------- ##
 
 ## --------------------------------------- Skrevet af Magnus og Bjørn -------------------------------- ##
@@ -27,7 +43,18 @@ async def insert_dynamic_data(
         templateFile: UploadFile = File(...),
         contextFile: UploadFile = File(...),
         pdfName: str = Form("Invoice"),
+        x_api_key: str = Header(None),
+        
     ):
+    logging.info("Received request to /insert-dynamic-data/")
+
+     # Validate API key
+    if not x_api_key or not validate_api_key(x_api_key):
+        raise HTTPException(status_code=401, detail="Unauthorized: Invalid API Key")
+
+    logging.info("API Key validated")
+
+
     ## -------------------------------------- Skrevet af Magnus -------------------------------------- ##
     # Ekstrahere context-dictionary og imageURLs-liste fra .json filer
     context: dict = json.loads(contextFile.file.read())
