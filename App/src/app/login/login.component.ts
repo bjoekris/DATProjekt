3fbbcd0a5948b66bdf23f0f78e6b00457f2c662a
KaderKivrak <119049854+KaderKivrak@users.noreply.github.com>
2025-01-01 19:50:27 +0100

    lidt oprydning i login

--- App/src/app/login/login.component.ts
+++ App/src/app/login/login.component.ts
@@ -1,37 +1,3 @@
-// import { Component } from '@angular/core';
-// import { Router, RouterModule } from '@angular/router';
-// import { FormsModule } from '@angular/forms';
-// import { AuthService } from '../auth.service';
-
-
-// @Component({
-//   selector: 'app-login',
-//   standalone: true,
-//   imports: [FormsModule, RouterModule],
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css'],
-// })
-// export class LoginComponent {
-//   email: string = '';
-//   password: string = '';
-
-//   constructor(private authService: AuthService, private router: Router) {}
-
-//   onLogin() {
-//     this.authService.login(this.email, this.password).subscribe(
-//       (response) => {
-//         console.log('Login succesful', response);
-
-//         //naviger til:
-//         this.router.navigate(['/invoice']);
-//       },
-//       (error) => {
-//         console.error('Login failed', error);
-//       }
-//     );
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
