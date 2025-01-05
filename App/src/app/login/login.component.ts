741980071331e315834a5c0455ccf103acdf8be6
KaderKivrak <119049854+KaderKivrak@users.noreply.github.com>
2025-01-05 13:20:46 +0100

    login error handling virker

--- App/src/app/login/login.component.ts
+++ App/src/app/login/login.component.ts
@@ -21,15 +21,17 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
+    this.errorMessage = '';
+
     this.authService.login(this.email, this.password).subscribe(
       (response) => {
         console.log('Login successful', response);
-        this.authService.saveToken(response.token);
         this.router.navigate(['/invoice']);
       },
       (error) => {
         console.error('Login failed', error);
-        this.errorMessage = 'Login failed';
+        this.errorMessage = 
+        error.error.message || 'Login failed';
       }
     );
   }
